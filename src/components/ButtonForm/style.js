52f476dec0f7da2b89e9e84827188c106015c1cb
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-03-24 11:20:01 -0300

    FEAT - Layout do componente ButtonForm

---
+++ src/components/ButtonForm/style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: var(--text-white);
+    font-size: 1rem;
+    width: 14rem;
+    background-color: var(--primary);
+    border: none;
+    margin: 3.5rem auto 2rem;
+    padding: 0.75rem 0;
+`;
